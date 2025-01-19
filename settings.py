b58a302361da6e9208d230c1944aea3274c35fbd
Lavanya08M <Lavanya08M@github.com>
2025-01-18 21:43:35 -0500

    init method in sttings is corrected and added ship instance to the game

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
     A class to store all settings for Alien Invasion.
     """
 
-    def __inti__(self):
+    def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
