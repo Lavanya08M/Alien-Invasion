97f0c153ad888ca78b302412606345d01ef1f56b
Lavanya08M <Lavanya08M@github.com>
2025-01-27 12:50:41 -0500

    Added logic for movement of ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,8 +16,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -31,8 +32,6 @@ class AlienInvasion:
             self.ship.update()
             self._update_screen()
             
-            
-    
     def _check_events(self):
         """Respond to keypresses and mouse events"""
         # Watch for keyboard and mouse events.
@@ -40,15 +39,31 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)   
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+                
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+    
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     
     def _update_screen(self):
--- ship.py
+++ ship.py
@@ -19,10 +19,14 @@ class Ship:
 
         # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
+        # Store a decimal value for the ship's vertical position.
+        self.y = float(self.rect.y)
 
         # Movement flag
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
     
     def update(self):
         """Update the ship's position based on the movement flags."""
@@ -31,9 +35,14 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom <self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -=  self.settings.ship_speed
 
-        # update rect object from self.x
+        # update rect object from self.x and self.y
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """ Draw the ship at its current location. """
